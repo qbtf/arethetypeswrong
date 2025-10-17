e2935579c3f5a827ca38f9e858bf8084dca31895
qbtf <qbtf@abc.xyz>
2025-10-17 16:30:39 +0800

    Fix path

--- packages/web/vite.config.js
+++ packages/web/vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
 import { execSync } from 'child_process';
 
+const CI = 'CI' in process.env;
+
 export default defineConfig(() => ({
   root: './src',
+  base: CI ? '/arethetypeswrong' : '',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
